a04c729b875bfed0db6d3651fc301bc3737b1153
Nael Louis <nael.louis1002@gmail.com>
2020-11-15 16:05:53 -0500

    script.js has been added to the project

---
+++ script/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', setup);
+
+function setup() {
+    document.querySelector(button).addEventListener('click', fetchData);
+}
+
+function fetchData(e) {
+    let url = ''
+}
